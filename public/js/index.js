91b4fb61bbad2daf0a694406edcb876e1879927e
Andy0414 <pjh8667@naver.com>
2019-06-08 20:26:23 +0900

    UPDATE design

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,13 @@
 const go = document.getElementById("go")
 const codeInput = document.getElementById("codeInput")
+const nameInput = document.getElementById("nameInput")
 const chatInput = document.getElementById("chatInput")
 const chatBox = document.getElementById("chatBox")
+const chatSend = document.getElementById("chatSend")
 const clear = document.getElementById("clear")
 const boom = document.getElementById("boom")
+const app = document.getElementById("app")
+const chat = document.getElementById("chat")
 
 const socket = io()
 
@@ -11,20 +15,34 @@ var myCode = null
 var myName = null
 
 go.addEventListener("click", e => {
-    document.getElementById("app").style.display = "none"
-    document.getElementById("chat").style.display = "flex"
+    myCode = codeInput.value
+    myName = nameInput.value
+    if (myCode && myName) {
+        app.style.opacity = 0
+        chat.style.zIndex = 20
+        chat.style.opacity = 1
+        chat.style.transform = "scale(1)"
 
-    myCode = codeInput.value.split(":")[0]
-    myName = codeInput.value.split(":")[1] || "NONAME"
-    socket.emit("joinRoom", {
-        roomCode: myCode
-    })
+        socket.emit("joinRoom", {
+            roomCode: myCode
+        })
+        chatInput.addEventListener("blur", e => {
+            chatInput.focus()
+        })
+    }
 })
-codeInput.addEventListener("keypress", e => {
+var gotoChat = (e) => {
     if (e.keyCode == 13) go.click()
-})
+}
+codeInput.addEventListener("keypress", gotoChat)
+nameInput.addEventListener("keypress", gotoChat)
 chatInput.addEventListener("keypress", e => {
-    if (chatInput.value.trim() && e.keyCode == 13) {
+    if (e.keyCode == 13) chatSend.click()
+})
+
+chatSend.addEventListener("click", e => {
+    chatInput.focus()
+    if (chatInput.value.trim()) {
         var div = document.createElement("div")
         var alignDiv = document.createElement("div")
         div.classList.add("chatBox__item")
@@ -32,6 +50,7 @@ chatInput.addEventListener("keypress", e => {
         div.innerText = chatInput.value
         alignDiv.innerText = myName
         alignDiv.style.textAlign = "right"
+        div.style.backgroundColor = "black"
         alignDiv.appendChild(div)
 
         chatBox.appendChild(alignDiv)
@@ -56,15 +75,58 @@ boom.addEventListener("click", e => {
 socket.on("boomRoom", data => {
     window.location = ""
 })
+const colors = ["#ec407a",
+    "#ab47bc",
+    "#7e57c2",
+    "#5c6bc0",
+    "#42a5f5",
+    "#29b6f6",
+    "#26c6da",
+    "#26a69a",
+    "#66bb6a",
+    "#9ccc65",
+    "#d4e157",
+    "#ffee58",
+    "#ffca28",
+    "#ffa726",
+    "#ff7043",
+    "#8d6e63",
+    "#78909c"
+]
+for (let i = 0; i < colors.length; i++) {
+    let rand = Math.floor(Math.random() * colors.length)
+    let tmp = colors[i]
+    colors[i] = colors[rand]
+    colors[rand] = tmp
+}
+var nameList = []
+
 socket.on("getMsg", data => {
+    var colorIndex = nameList.findIndex(x => x.name == data.name)
+    if (colorIndex == -1) {
+        nameList.push({
+            name: data.name,
+            color: colors[nameList.length]
+        })
+        colorIndex = nameList.length - 1
+    }
+
     var div = document.createElement("div")
     var alignDiv = document.createElement("div")
     div.classList.add("chatBox__item")
-
     div.innerText = data.msg
+    console.log(nameList)
+    div.style.backgroundColor = nameList[colorIndex].color
+
+    var name = document.createElement("div")
+    name.classList.add("name")
+    name.innerText = data.name
+    name.style.color = nameList[colorIndex].color
+    alignDiv.appendChild(name)
+
+    alignDiv.classList.add("notMe")
     alignDiv.style.textAlign = "left"
     alignDiv.appendChild(div)
-    alignDiv.appendChild(document.createTextNode(data.name))
 
     chatBox.appendChild(alignDiv)
     chatBox.scrollTop = chatBox.scrollHeight
